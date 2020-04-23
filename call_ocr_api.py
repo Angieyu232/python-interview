d7a1aa3ea5275e8714d6f5b17713183c88ba9203
Angie Yu <58761050+Angieyu232@users.noreply.github.com>
2020-04-23 09:56:05 -0700

    using Azuring API for recognizing text from image

---
+++ call_ocr_api.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import requests
+import time
+# If you are using a Jupyter notebook, uncomment the following line.
+# %matplotlib inline
+# import matplotlib.pyplot as plt
+# from matplotlib.patches import Polygon
+# from PIL import Image
+# from io import BytesIO
+
+
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+
+
+
+# Add your Computer Vision subscription key and endpoint to your environment variables.
+if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+else:
+    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+
+if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+
+text_recognition_url = endpoint + "vision/v2.1/read/core/asyncBatchAnalyze"
+
+# Set image_url to the URL of an image that you want to analyze.
+image_url = "https://upload.wikimedia.org/wikipedia/commons/d/dd/Cursive_Writing_on_Notebook_paper.jpg"
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+parameters  = {'language':'en', 'detectOrientation':True}
+
+data = {'url': image_url}
+response = requests.post(
+    text_recognition_url, headers=headers, params=parameters, json=data)
+response.raise_for_status()
+
+# Extracting text requires two API calls: One call to submit the
+# image for processing, the other to retrieve the text found in the image.
+
+# Holds the URI used to retrieve the recognized text.
+operation_url = response.headers["Operation-Location"]
+
+# The recognized text isn't immediately available, so poll to wait for completion.
+analysis = {}
+poll = True
+
+while (poll):
+    response_final = requests.get(
+        response.headers["Operation-Location"], headers=headers)
+    analysis = response_final.json()
+    print(analysis)
+    time.sleep(1)
+    if ("recognitionResults" in analysis):
+        poll = False
+    if ("status" in analysis and analysis['status'] == 'Failed'):
+        poll = False
+
+
+
+if analysis['status'] == 'Succeeded':
+  # using a recursive function to retrieve all values of key word 'text
+  # texts_res = extract_values(analysis, 'text')
+  # the is hard code to access the nested key value pair of text result
+  texts_res = analysis['recognitionResults'][0]['lines'][0]['text']
+  print(texts_res)
