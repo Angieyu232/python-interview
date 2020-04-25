2b00f053992f82458085617110902dbaa75c6601
Angie Yu <58761050+Angieyu232@users.noreply.github.com>
2020-04-24 22:08:40 -0700

    Update coin_change.py

--- coin_change.py
+++ coin_change.py
@@ -33,6 +33,13 @@ class Solution:
 
         return dp[amount] if dp[amount] != float('inf') else -1
 
-
-
+  #corner case, if there is no way to solve the change, returnn 0
+    def coin_change2(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
+        return dp[amount]
 
